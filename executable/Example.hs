8e01ec0226e9919f84e78585c8dd163c6835d4b4
Chris Mahon <cnmahon@gmail.com>
2014-06-04 12:51:18 +1000

    Added example using state api

--- executable/Example.hs
+++ executable/Example.hs
@@ -1,15 +1,30 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
 module Main where
 
 import           Control.Category
 import           Control.Monad           
-import           Data.Monoid                      
+import           Data.Monoid  
+import           Data.Typeable                    
 import           MVC
+import           MVC.Event                        hiding (handleEvent)
+import           MVC.Model                        
 import           MVC.Prelude
 import           MVC.Service
 import qualified Pipes.Prelude                    as P
 import           Prelude                          hiding (id,(.))
 
 -----------------------------------------------------------------------------
+-- Example 1
 
 msA :: ManagedService String String
 msA = toManagedService $ fromPipe Unbounded Unbounded $ forever $ await >>= \e -> when (e == "do") $ yield "done" 
@@ -27,5 +42,74 @@ external = do
 model :: Model () String String
 model = asPipe (P.takeWhile (/= "quit"))
 
+-----------------------------------------------------------------------------
+-- Example 2
+
+-- Services: SomeEvent-wrapped stdin & stdout
+
+instance Event String
+
+external' :: Managed (View SomeEvent, Controller SomeEvent)
+external' = do
+  c <- stdinLines
+  return (contramap show stdoutLines,fmap SomeEvent c)
+
+-- App Service: Incrementer
+
+data TestAppService a = TestAppService
+  { _testCount :: Int
+  }
+
+newTestAppService :: AppStateAPI (TestAppService a) -> SomeAppService a
+newTestAppService api = SomeAppService 0 api (TestAppService 0)
+
+instance AppService (TestAppService a) where
+  type AppState (TestAppService a) = a
+  data AppStateAPI (TestAppService a) = TestAppServiceAPI { _testTest :: a -> a}
+  processEvent _ e
+    | Just ("inc"::String) <- fromEvent e = do
+        api <- getAppStateAPI
+        modifyAppState (_testTest api)
+        noEvents
+    | otherwise = noEvents
+
+-- App Service: Logger
+
+data Msg = Msg String deriving (Typeable,Show)
+
+instance Event Msg
+
+data LogAppService a = LogAppService
+
+instance AppService (LogAppService a) where
+  type AppState (LogAppService a) = a
+  data AppStateAPI (LogAppService a) = LogAppServiceAPI
+  processEvent _ e = return [release . Msg $ show e]
+
+newLogAppService :: SomeAppService a
+newLogAppService = SomeAppService 0 LogAppServiceAPI LogAppService
+
+-- App services
+
+appServices :: [SomeAppService Int]
+appServices = initialiseAppServices
+  [ newTestAppService (TestAppServiceAPI (+1)) 
+  , newLogAppService
+  ]
+
+-- Model
+
+model' :: Model Int SomeEvent SomeEvent
+model' = asPipe (P.takeWhile (not . done)) >>> asPipe (runAppModel appServices handleEvent)
+  where 
+  done e
+    | Just ("quit" :: String) <- fromEvent e = True 
+    | otherwise = False
+
+-- -----------------------------------------------------------------------------
+-- Main
+
 main :: IO ()
-main = runMVC () model external
+main = do
+  print =<< runMVC () model external
+  print =<< runMVC 0 model' external'
